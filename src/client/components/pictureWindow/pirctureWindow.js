9294b26b9c41d1d8d7fb502e9f872e2b685c267d
milkcan <milkcanbass@gmail.com>
2020-01-06 00:07:38 -0500

    upload image function

--- src/client/components/pictureWindow/pirctureWindow.js
+++ src/client/components/pictureWindow/pirctureWindow.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
-import image from '../../assets/sample.png';
 import './pirctureWindow.scss';
+import { storage } from '../../../firebase/firebaseConfig';
 
 class PictureWindow extends Component {
+  state = {
+    image: null,
+    url: '',
+    progress: 0,
+  };
+
+  handleChange = (e) => {
+    if (e.target.files[0]) {
+      const image = e.target.files[0];
+      console.log(image);
+      this.setState(() => ({ image }));
+    }
+  };
+
+  handleUpload = () => {
+    const { image } = this.state;
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        console.log(snpashot);
+
+        const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        this.setState({ progress });
+      },
+      (error) => {
+        // error function ....
+        console.log(error);
+      },
+      () => {
+        // complete function ....
+        storage
+          .ref('images')
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            console.log(url);
+            this.setState({ url });
+          });
+      },
+    );
+  };
+
   render() {
     return (
-      <div className="picWidWrapper">
-        <img src={image} alt="upload image" />
+      <div>
+        <progress value={this.state.progress} max="100" />
+        <br />
+        <input type="file" onChange={this.handleChange} />
+        <button onClick={this.handleUpload}>Upload</button>
+        <br />
+        <img
+          src={this.state.url || 'http://via.placeholder.com/400x300'}
+          alt="Uploaded images"
+          height="300"
+          width="400"
+        />
       </div>
     );
   }
