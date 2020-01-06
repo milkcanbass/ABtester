98ab66970e66e9f0d90151527107c0ba8583f6c5
milkcan <milkcanbass@gmail.com>
2020-01-05 20:41:05 -0500

    test

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: 'AIzaSyBWjaVMczAYm5dvkaCiNzDmK90o-33HTB4',
+  authDomain: 'abtester-5b109.firebaseapp.com',
+  databaseURL: 'https://abtester-5b109.firebaseio.com',
+  projectId: 'abtester-5b109',
+  storageBucket: 'abtester-5b109.appspot.com',
+  messagingSenderId: '910039906708',
+  appId: '1:910039906708:web:ae3144b57e93f5087b3781',
+  measurementId: 'G-4R58QN65DQ',
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
