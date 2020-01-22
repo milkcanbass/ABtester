b9ebaa4a420c2d6214a42b93a635600d752873fd
milkcan <milkcanbass@gmail.com>
2020-01-22 14:18:44 -0500

    adjust layout for iphone se

--- src/client/App.js
+++ src/client/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './app.scss';
+import './_app.scss';
 
 // Components
 import NotFound from './pages/notFound/notFound';
--- src/client/components/topComponent/topComponent.js
+++ src/client/components/topComponent/topComponent.js
@@ -1,33 +1,28 @@
 import React from 'react';
-import img1 from '../../assets/topImage.jpg';
 import './topComponent.style.scss';
 
 // component
 import PicturesSection from '../picturesSection/picturesSection';
 
 const TopComponent = () => (
-  <div className="topComponentHighLevel">
-    <div className="topComponentWrapper">
-      <div className="topImage">
-        <div className="imageContainer">
-          <img src={img1} alt="topImage" />
-        </div>
-        <div className="textSection">
-          <div className="title">Which do you like?</div>
-          <div className="description">
-            <h3>
-              Free AB test service. We will create a private survey page. The only thing you need to
-              do is to upload 2 images and title, and share the url to your friends.
-            </h3>
-          </div>
+  <div className="topComponentWrapper">
+    <div className="topImage">
+      <div className="imageContainer" />
+      <div className="textSection">
+        <div className="title">Which do you like?</div>
+        <div className="description">
+          <h3>
+            Free AB test service. We will create a private survey page. The only thing you need to
+            do is to upload 2 images and title, and share the url to your friends.
+          </h3>
         </div>
       </div>
-      <div className="topPicSection">
-        <div className="topPicBackground">
-          <h3>Upload images.(max size 1MB)</h3>
-          <div className="picSecWrapper">
-            <PicturesSection />
-          </div>
+    </div>
+    <div className="topPicSection">
+      <div className="topPicBackground">
+        <h3>Upload images.(max size 1MB)</h3>
+        <div className="picSecWrapper">
+          <PicturesSection />
         </div>
       </div>
     </div>
